b59f35fb248867bee49bef429455732d282c74a6
fadzruletw <fadzrul.entertheworld@gmail.com>
2024-04-05 01:32:01 +0800

    First Commit
    Home Landing page

---
+++ footer.php
@@ -0,0 +1,57 @@
+    <footer class="site-footer">
+      <div class="container">
+        <div class="row mb-5">
+          <div class="col-md-4">
+            <h3>Phone Support</h3>
+            <p>24/7 Call us now.</p>
+            <p class="lead"><a href="tel://">+60 377770808</a></p>
+          </div>
+          <div class="col-md-4">
+            <h3>Connect With Us</h3>
+            <p>We are socialized. Follow us</p>
+            <p>
+              <a href="http://www.facebook.com" target="_blank" class="pl-0 p-3"><span class="fa fa-facebook"></span></a>
+              <a href="http://www.twitter.com" target="_blank" class="p-3"><span class="fa fa-twitter"></span></a>
+              <a href="http://www.instagram.com" target="_blank" class="p-3"><span class="fa fa-instagram"></span></a>
+              <a href="http://www.vimeo.com" target="_blank" class="p-3"><span class="fa fa-vimeo"></span></a>
+              <a href="http://www.youtube.com" target="_blank" class="p-3"><span class="fa fa-youtube-play"></span></a>
+            </p>
+          </div>
+          <div class="col-md-4">
+            <h3>Connect With Us</h3>
+            <p>Send us an email to : <a href="mailto:support@hotelsunshine.com"><i>support@hotelsunshine.com</i></a> </p>
+            <form action="#" class="subscribe">
+              <div class="form-group">
+                <button type="submit"><span class="ion-ios-arrow-thin-right"></span></button>
+                <input type="email" class="form-control" placeholder="Enter email">
+              </div>
+              
+            </form>
+          </div>
+        </div>
+        <div class="row justify-content-center">
+          <div class="col-md-7 text-center">
+            &copy; Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved
+          </div>
+        </div>
+      </div>
+    </footer>
+    <!-- END footer -->
+    
+    <!-- loader -->
+    <div id="loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#f4b214"/></svg></div>
+
+    <script src="js/jquery-3.2.1.min.js"></script>
+    <script src="js/jquery-migrate-3.0.0.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/jquery.waypoints.min.js"></script>
+    <script src="js/jquery.stellar.min.js"></script>
+
+    <script src="js/jquery.magnific-popup.min.js"></script>
+    <script src="js/magnific-popup-options.js"></script>
+
+    <script src="js/main.js"></script>
+  </body>
+</html>
---
+++ index.php
@@ -0,0 +1,72 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Hotel Reservation System</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link href="https://fonts.googleapis.com/css?family=Playfair+Display:400,700,900|Rubik:300,400,700" rel="stylesheet">
+
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/animate.css">
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+
+    <link rel="stylesheet" href="fonts/ionicons/css/ionicons.min.css">
+    <link rel="stylesheet" href="fonts/fontawesome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/magnific-popup.css">
+
+    <!-- Theme Style -->
+    <link rel="stylesheet" href="css/style.css">
+  </head>
+  <body>
+    
+    <header role="banner">
+     
+      <nav class="navbar navbar-expand-md navbar-dark bg-light">
+        <div class="container">
+          <a class="navbar-brand" href="index.php">Hotel Sunshine</a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample05" aria-controls="navbarsExample05" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+
+          <div class="collapse navbar-collapse navbar-light" id="navbarsExample05">
+            <ul class="navbar-nav ml-auto pl-lg-5 pl-0">
+              <li class="nav-item">
+                <a class="nav-link active" href="index.php">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#featuredrooms">Rooms</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="mailto:support@hotelsunshine.com">Contact</a>
+              </li>
+              <li class="nav-item cta">
+                <a class="nav-link" href="login.php"><span>Login</span></a>
+              </li>
+            </ul>
+            
+          </div>
+        </div>
+      </nav>
+    </header>
+    <!-- END header -->
+
+    <section class="site-hero overlay" data-stellar-background-ratio="0.5" style="background-image: url(images/big_image_1.jpg);">
+      <div class="container">
+        <div class="row align-items-center site-hero-inner justify-content-center">
+          <div class="col-md-12 text-center">
+
+            <div class="mb-5 element-animate">
+              <h1>Welcome to Hotel Sunshine</h1>
+              <p>Get a room with our Hotel Reservation System</p>
+              <p><a href="customer.php" class="btn btn-primary">Book Now</a></p>
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- END section -->
+
+   
+<?php include 'footer.php' ?>
